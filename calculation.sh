5ba27154f06861024c0c35fb3ba512ccf80c9409
root <root@DESKTOP-9PQF3MH.localdomain>
2020-05-22 21:49:21 +0530

    Added calculation.sh

---
+++ calculation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+function add()
+{
+echo $(($1+$2))
+}
+
+function mul()
+{
+echo $(($1*$2))
+}
+
+function div()
+{
+echo $(($1/$2))
+}
+
+function sub()
+{
+echo $(($1-$2))
+}
+result= $(mul 2 3)
